fb594b35f63cae9c993f59bef3c6f80be0c87302
aeirya <aeirya@gmail.com>
2020-07-22 21:09:22 +0430

    added worker class

---
+++ workers.py
@@ -0,0 +1,17 @@
+class Worker:
+    def __init__(self, id , fname, lname, phone, skills, score):
+        self.id = id
+        self.fname = fname
+        self.lname = lname
+        self.phone = phone
+        self.skills = skills
+        self.score = score
+
+class WorkerManager:
+    
+    def __init__(self, data):
+        self.data = data
+        self.workers = data.allWorkers()
+
+    def add_worker(self, worker : Worker):
+        self.data.addWorker(worker.fname, worker.lname, worker.phone, worker.skills)
