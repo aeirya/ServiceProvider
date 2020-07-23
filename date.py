c01d778284af060ce0e3fa4c7e50a70eed31f820
aeirya <aeirya@gmail.com>
2020-07-23 17:02:25 +0430

    added date lib

--- date.py
+++ date.py
@@ -1,3 +1,11 @@
+import datetime
+
 class Date:
-    def __init__(string : str):
-        
+    def __init__(self):
+        self.format = "%m/%d/%Y, %H:%M:%S"
+
+    def to_date(self, string : str):
+        return datetime.strptime(string, self.format)
+
+    def to_string(self, date):
+        return date.strftime(self.format)
